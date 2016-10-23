2b9f59acae1b7e616bea2a841d66fb9da88e74c3
Joël Luijmes <joelluijmes@gmail.com>
2016-10-23 17:33:39 +0200

    changed encoding to string instead of array

--- hasm/Instruction.cs
+++ hasm/Instruction.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using NLog;
 using OfficeOpenXml;
 
@@ -7,7 +9,7 @@ namespace hasm
 {
     internal class Instruction
     {
-        public Instruction(string grammar, string description, string semantic, string[] encoding)
+        public Instruction(string grammar, string description, string semantic, string encoding)
         {
             Grammar = grammar;
             Description = description;
@@ -18,8 +20,8 @@ namespace hasm
         public string Grammar { get; }
         public string Description { get; }
         public string Semantic { get; }
-        public int Count => Encoding.Length*4;
-        public string[] Encoding { get; }
+        public int Count => Encoding.Length/8;
+        public string Encoding { get; }
 
         public override string ToString() => $"{Grammar.Split(' ')[0]} ({Description})";
 
@@ -30,37 +32,18 @@ namespace hasm
             var description = row[rowIndex, 2].GetValue<string>();
             var semantic = row[rowIndex, 3].GetValue<string>();
             
-            var strencoding = ParseToString(row[rowIndex, 4, rowIndex, 9].Value as object[,]);
-            var encoding = ParseEncoding(strencoding);
+            var encoding = ParseToString(row[rowIndex, 4, rowIndex, 9].Value as object[,]);
             
             return new Instruction(grammar, description, semantic, encoding);
         }
 
-        private static string[] ParseToString(object[,] multi)
+        private static string ParseToString(object[,] multi)
         {
-            var strarr = new string[multi.GetLength(1)];
+            var builder = new StringBuilder();
             for (var i = 0; i < multi.GetLength(1); ++i)
-                strarr[i] = multi[0, i]?.ToString();
-
-            return strarr;
-        }
-
-        private static string[] ParseEncoding(IList<string> input)
-        {
-            if (input.Count != 6)
-                throw new ArgumentException("Count of input must be 6", nameof(input));
-
-            if (input[0] != null)
-            {
-                return input[4] == null
-                    ? new[] {input[0], input[1], input[2], input[3]}
-                    : new[] {input[0], input[1], input[2], input[3], input[4], input[5]};
-            }
-
-            if (input[4] == null)
-                throw new ArgumentException("Invalid encoding input", nameof(input));
-
-            return new[] {input[4], input[5]};
+                builder.Append(multi[0, i]);
+            
+            return builder.ToString();
         }
     }
 }
