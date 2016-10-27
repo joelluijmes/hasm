a7a1265481880d76ef16222650046d90ef4b7827
Joël Luijmes <joelluijmes@gmail.com>
2016-10-27 16:06:20 +0200

    fixed rules

--- HasmParser/HasmGrammer.Rules.cs
+++ HasmParser/HasmGrammer.Rules.cs
@@ -4,8 +4,9 @@ namespace hasm.Parsing
 {
 	public sealed partial class HasmGrammar
 	{
-		public static readonly ValueRule<string> Operand = Text("operand", MatchWhile(Label));
-		public static readonly ValueRule<string> ListingLabel = FirstValue<string>("label", Text(MatchWhile(Label)) + MatchChar(':'));
-
+		public static readonly ValueRule<string> Operand = Text("operand", Label);
+		public static readonly ValueRule<string> ListingLabel = FirstValue<string>("label", Text(Label) + MatchChar(':'));
+		public static readonly ValueRule<string> ListingInstruction = FirstValue<string>("instruction", Text(MatchWhile(MatchChar(';').Not + MatchAnyChar())));
+		public static readonly ValueRule<string> ListingComment = FirstValue<string>("comment", MatchChar(';') + Text(MatchWhile(MatchAnyChar())));
 	}
 }
