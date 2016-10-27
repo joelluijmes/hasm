514550cca6a7a7d64f054c3ad6fb248402d61b8a
Joël Luijmes <joelluijmes@gmail.com>
2016-10-27 14:49:34 +0200

    added menu

---
+++ Hasm/Instruction.cs
@@ -0,0 +1,10 @@
+﻿namespace hasm
+{
+	internal sealed class Instruction
+	{
+		public string Label { get; set; }
+		public int Address { get; set; }
+		public string Input { get; set; }
+		public string Comment { get; set; }
+	}
+}
--- Hasm/Program.cs
+++ Hasm/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using Fclp;
 using hasm.Parsing;
 using NLog;
 using ParserLib.Evaluation;
@@ -11,59 +12,90 @@ namespace hasm
 	internal class Program
 	{
 		private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+		private static readonly Dictionary<string, OperandType> _defines = new Dictionary<string, OperandType>
+		{
+			["REG"] = OperandType.FirstGeneralRegister,
+			["DST"] = OperandType.FirstGeneralRegister,
+			["REG1"] = OperandType.FirstGeneralRegister,
+			["REG2"] = OperandType.SecondGeneralRegister,
+			["SRC"] = OperandType.SecondGeneralRegister,
 
+			["SPC"] = OperandType.FirstSpecialRegister,
+			["SPC1"] = OperandType.FirstSpecialRegister,
+			["SPC2"] = OperandType.SecondSpecialRegister,
+
+			["IMM6"] = OperandType.Immediate6,
+			["IMM8"] = OperandType.Immediate8,
+			["IMM12"] = OperandType.Immediate12,
+
+			["c"] = OperandType.BranchIf,
+			["PAIR + IMM6"] = OperandType.PairOffset
+		};
+		
 		private static void Main(string[] args)
 		{
 			try
 			{
 				AppDomain.CurrentDomain.UnhandledException += UnhandledException;
 
-				ExecuteIfDebug(() =>
+				if (Debugging)
 				{
 					var consoleRule = LogManager.Configuration.LoggingRules.First(r => r.Targets.Any(t => t.Name == "console"));
 					consoleRule.EnableLoggingForLevel(LogLevel.Debug);
-				});
-
-				var defines = new Dictionary<string, OperandType>
-				{
-					["REG"] = OperandType.FirstGeneralRegister,
-					["DST"] = OperandType.FirstGeneralRegister,
-					["REG1"] = OperandType.FirstGeneralRegister,
-					["REG2"] = OperandType.SecondGeneralRegister,
-					["SRC"] = OperandType.SecondGeneralRegister,
-
-					["SPC"] = OperandType.FirstSpecialRegister,
-					["SPC1"] = OperandType.FirstSpecialRegister,
-					["SPC2"] = OperandType.SecondSpecialRegister,
-
-					["IMM6"] = OperandType.Immediate6,
-					["IMM8"] = OperandType.Immediate8,
-					["IMM12"] = OperandType.Immediate12,
-
-					["c"] = OperandType.BranchIf,
-					["PAIR + IMM6"] = OperandType.PairOffset
-				};
-
-				var parser = new HasmParser(defines);
-
-				while (true)
-				{
-					Console.Write("Enter instruction: ");
-					var line = Console.ReadLine();
-					if (string.IsNullOrEmpty(line))
-						break;
-
-					var rule = parser.FindRule(line);
-
-					var encoded = rule.FirstValue<int>(line);
-					_logger.Info($"Parsed {line} to encoding {encoded:X3}");
-
-					Console.WriteLine();
 				}
+				else
+				{
+					var commandParser = new FluentCommandLineParser<ApplicationArguments>();
+					commandParser.Setup(a => a.InputFile)
+						.As('i', "input")
+						.WithDescription("\tInput file to be assembled");
+					commandParser.Setup(a => a.OutputFile)
+						.As('o', "output")
+						.WithDescription("Output file of the assembler");
+					commandParser.Setup(a => a.LiveMode)
+						.As('l', "live-mode")
+						.WithDescription("Use live mode");
+					commandParser.SetupHelp("?", "help")
+						.WithHeader("Invalid usage: ")
+						.Callback(c => Console.WriteLine(c));
+
+					var result = commandParser.Parse(args);
+					if (result.HasErrors || result.EmptyArgs)
+					{
+						commandParser.HelpOption.ShowHelp(commandParser.Options);
+						return;
+					}
+
+					var arguments = commandParser.Object;
+					if (arguments.LiveMode)
+						MenuLiveMode();
+				}
+				
+				MenuLiveMode();
 			}
 			catch (Exception e)
 			{
 				UnhandledException(e);
+			}
+		}
+
+		private static void MenuLiveMode()
+		{
+			var parser = new HasmParser(_defines);
+
+			while (true)
+			{
+				Console.Write("Enter instruction: ");
+				var line = Console.ReadLine();
+				if (string.IsNullOrEmpty(line))
+					break;
+
+				var rule = parser.FindRule(line);
+
+				var encoded = rule.FirstValue<int>(line);
+				_logger.Info($"Parsed {line} to encoding {encoded:X3}");
+
+				Console.WriteLine();
 			}
 		}
 
@@ -86,12 +118,18 @@ namespace hasm
 			Environment.Exit(-1);
 		}
 
-		private static void ExecuteIfDebug(Action action)
-		{
+		private static bool Debugging =>
 #if DEBUG
-			if (Debugger.IsAttached)
-				action();
+			Debugger.IsAttached;
+#else
+			false;
 #endif
+
+		private class ApplicationArguments
+		{
+			public string InputFile { get; set; }
+			public string OutputFile { get; set; }
+			public bool LiveMode { get; set; }
 		}
 	}
 }
