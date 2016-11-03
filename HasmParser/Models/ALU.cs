6a19ec3d754fe4474833d99e21895bf5480d5173
Joël Luijmes <joelluijmes@gmail.com>
2016-11-03 11:22:22 +0100

    operation parsing into AluOperation (enum)

--- HasmParser/Models/ALU.cs
+++ HasmParser/Models/ALU.cs
@@ -1,8 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using hasm.Parsing.Grammars;
 using NLog;
-using ParserLib;
 using ParserLib.Evaluation;
 
 namespace hasm.Parsing.Models
@@ -10,6 +10,16 @@ namespace hasm.Parsing.Models
     public sealed class ALU
     {
         private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+        private static readonly Dictionary<string, AluOperation> _operations = new Dictionary<string, AluOperation>
+        {
+            ["-"] = AluOperation.Minus,
+            ["+"] = AluOperation.Plus,
+            ["&"] = AluOperation.And,
+            ["|"] = AluOperation.Or,
+            ["^"] = AluOperation.Xor,
+            ["nop"] = AluOperation.Clear,
+            [""] = AluOperation.Clear
+        };
 
         public string Target { get; set; }
         public string Left { get; set; }
@@ -21,13 +31,17 @@ namespace hasm.Parsing.Models
         {
             input = new Regex("\\s+").Replace(input, "");
             if (string.IsNullOrEmpty(input))
-                    throw new ArgumentNullException(nameof(input));
+                throw new ArgumentNullException(nameof(input));
 
+            var alu = new ALU();
             var parsed = MicroHasmGrammar.Alu.ParseTree(input);
 
             var target = parsed.FirstValueByNameOrDefault<string>("target");
             var left = parsed.FirstValueByNameOrDefault<string>("left");
             var op = parsed.FirstValueByNameOrDefault<string>("op");
+            if (op != null)
+                alu.Operation = _operations[op];
+
             var right = parsed.FirstValueByNameOrDefault<string>("right");
             var carry = parsed.FirstValueByNameOrDefault<string>("carry");
             var sp = parsed.FirstValueByNameOrDefault<string>("SP");
@@ -42,5 +56,12 @@ namespace hasm.Parsing.Models
 
     public enum AluOperation
     {
+        Clear = 0,
+        Minus = 1 << 1,
+        Plus = (1 << 1) | (1 << 0),
+        Xor = 1 << 2,
+        Or = (1 << 2) | (1 << 0),
+        And = (1 << 2) | (1 << 1),
+        Preset = (1 << 2) | (1 << 1) | (1 << 0)
     }
 }
