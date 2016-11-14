45bea811d7421b7ef353e202d5796a56dda1642f
Joël Luijmes <joelluijmes@gmail.com>
2016-11-14 09:12:32 +0100

    added files

---
+++ HasmParser/Encoding/TypeConverters/ImmediateConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using hasm.Parsing.Models;
+
+namespace hasm.Parsing.Encoding.TypeConverters
+{
+    internal sealed class ImmediateConverter : TypeConverter
+    {
+        public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+        {
+            var alu = context.Instance as ALU;
+            if (value is OperandConverter == false || alu == null)
+                return base.ConvertFrom(context, culture, value);
+
+            var right = (OperandConverter)value;
+            return !string.IsNullOrEmpty(right.Operand) && right.IsImmediate 
+                ? right.Value 
+                : 0;
+        }
+
+        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType) =>
+            sourceType == typeof(OperandConverter) || base.CanConvertFrom(context, sourceType);
+    }
+}
